ffdf0ae9201905ef7426bd4b9c2af30bcd43292b
emmanuel ronoh <eronoh036@gmail.com>
2024-08-03 18:08:09 +0300

    first commit

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -2,7 +2,14 @@ import React from "react";
 import howManyParks from "./parks/howManyParks";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  const parksCount = howManyParks(); // Call the function and store the result
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <p>{parksCount}</p> {/* Display the result of howManyParks */}
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
